daf5f28ef6234789618796a0e24c3ae3d0acf03c
Ye Yint Thu <thuyeyint018@gmail.com>
2024-01-09 17:30:37 +0630

    adding css and icons

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,7 +1,17 @@
 <template>
-    <div class="project" @click="showDetail=!showDetail">
-        <h3>{{project.title}}</h3>
+    <div class="project">
+        <div class="flexing">
+            <div>
+                <h3 @click="showDetail=!showDetail">{{project.title}}</h3>
+            </div>
+            <div>
+                <span class="material-icons">delete</span>
+                <span class="material-icons">edit</span>
+                <span class="material-icons">done</span>
+            </div>
+        </div>
         <p v-if="showDetail">{{project.detail}}</p>
+        
     </div>
 </template>
 
@@ -19,11 +29,25 @@ export default {
 <style>
     .project {
         padding: 20px;
-        background-color: #f2f2f2;
-        cursor: pointer;
+        background-color: #f2f2f2;   
         margin: 10px;
+        border-left: 6px solid crimson;
+        border-radius: 8px;
     }
     h3 {
+        cursor: pointer;
         color: indigo;
+    }
+    .flexing {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    span {
+        margin-left: 10px;
+    }
+    span:hover {
+        cursor: pointer;
+        color: #777;
     }
 </style>
