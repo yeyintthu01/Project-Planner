8c3f583665d1b31fd900ce075b907ddf485ab0b6
Ye Yint Thu <thuyeyint018@gmail.com>
2024-01-09 16:47:22 +0630

    fetch data from json-server and toggle show deatil

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="project" @click="showDetail=!showDetail">
+        <h3>{{project.title}}</h3>
+        <p v-if="showDetail">{{project.detail}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['project'],
+    data(){
+        return{
+            showDetail: false
+        }
+    }
+}
+</script>
+
+<style>
+    .project {
+        padding: 20px;
+        background-color: #f2f2f2;
+        cursor: pointer;
+        margin: 10px;
+    }
+    h3 {
+        color: indigo;
+    }
+</style>
