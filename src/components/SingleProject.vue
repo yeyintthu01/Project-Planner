5f8271d1691aaec27c9773340715a9a2bdd31f89
Ye Yint Thu <thuyeyint018@gmail.com>
2024-01-10 20:38:41 +0630

    complete feature at local and server

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,17 +1,16 @@
 <template>
-    <div class="project">
+    <div class="project" :class="{complete:project.complete}">
         <div class="flexing">
             <div>
                 <h3 @click="showDetail=!showDetail">{{project.title}}</h3>
             </div>
             <div>
-                <span class="material-icons">delete</span>
+                <span class="material-icons" @click="deleteProject">delete</span>
                 <span class="material-icons">edit</span>
-                <span class="material-icons">done</span>
+                <span class="material-icons" @click="updateProject">done</span>
             </div>
         </div>
         <p v-if="showDetail">{{project.detail}}</p>
-        
     </div>
 </template>
 
@@ -20,7 +19,38 @@ export default {
     props:['project'],
     data(){
         return{
-            showDetail: false
+            showDetail: false,
+            api:'http://localhost:3000/projects/'
+        }
+    },
+    methods: {
+        deleteProject(){
+             let deleteRoute=this.api+this.project.id;
+             fetch(deleteRoute,{method:"DELETE"})
+             .then(()=>{
+                this.$emit("delete",this.project.id)
+             })
+             .catch((err)=>{
+                console.log(err)
+             })
+        },
+        updateProject(){
+            let updateCompleteRoute=this.api+this.project.id;
+            fetch(updateCompleteRoute,{
+                method:"PATCH",
+                headers:{
+                    "Content-type":"application/json"
+                },
+                body:JSON.stringify({
+                    complete:!this.project.complete
+                })
+            })
+            .then(()=>{
+                this.$emit("complete",this.project.id)
+            })
+            .catch((err)=>{
+                console.log(err)
+            })
         }
     }
 }
@@ -49,5 +79,8 @@ export default {
     span:hover {
         cursor: pointer;
         color: #777;
+    }
+    .complete {
+        border-left-color: green;
     }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,12 @@
     <h1>Home</h1>
     <div v-if="projects">
       <div v-for="project in projects" :key="project.id">
-        <SingleProject :project="project"></SingleProject>
-    </div>
+        <SingleProject :project="project" @delete="deleteProject" @complete="completeProject"></SingleProject>
+      </div>
     </div>
 </template>
 
 <script>
-
-
 import SingleProject from '../components/SingleProject'
 export default {
   name: 'HomeView',
@@ -20,6 +18,19 @@ export default {
     return{
         projects:[]
     }
+  },
+  methods: {
+      deleteProject(id){
+        this.projects=this.projects.filter(project=>{
+          return project.id != id;
+        })
+    },
+      completeProject(id){
+          let findProject=this.projects.find(project=>{
+            return project.id === id;
+          })
+          findProject.complete= !findProject.complete
+      }
   },
   mounted(){
       fetch('http://localhost:3000/projects')
