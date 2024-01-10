ebb1151886dfb0d3722640d0adc65fdf986220c8
Ye Yint Thu <thuyeyint018@gmail.com>
2024-01-10 23:26:41 +0630

    filter nav with computed property

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
     <h1>Home</h1>
     <FilterNav @filterValue="current=$event" :current="current"></FilterNav>
     <div v-if="projects">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProject" :key="project.id">
         <SingleProject :project="project" @delete="deleteProject" @complete="completeProject"></SingleProject>
       </div>
     </div>
@@ -34,6 +34,21 @@ export default {
             return project.id === id;
           })
           findProject.complete= !findProject.complete
+      }
+  },
+  computed:{
+      filteredProject(){
+        if(this.current==='complete') {
+          return this.projects.filter((p)=>{
+            return p.complete
+          })
+        }
+        if(this.current==='ongoing') {
+          return this.projects.filter((p)=>{
+          return !p.complete
+          })
+        }
+        return this.projects;
       }
   },
   mounted(){
