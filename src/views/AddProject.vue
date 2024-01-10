9f0408a7c5292a1866f2b63e28b7ce7234840332
Ye Yint Thu <thuyeyint018@gmail.com>
2024-01-10 21:07:59 +0630

    add project form style and navbar style

---
+++ src/views/AddProject.vue
@@ -0,0 +1,57 @@
+<template>
+    <h1>Add Project</h1>
+    <form>
+        <label>Project Name</label>
+        <input type="text">
+        <label>Project Detail</label>
+        <input type="text">
+        <button>Add Project</button>
+    </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    form {
+    background: white;
+    padding: 20px;
+    border-radius: 10px;
+  }
+  label {
+    display: block;
+    color: #bbb;
+    text-transform: uppercase;
+    font-size: 14px;
+    font-weight: bold;
+    letter-spacing: 1px;
+    margin: 20px 0 10px 0
+  }
+  input {
+    padding: 10px;
+    border: 0;
+    border-bottom: 1px solid #ddd;
+    width: 100%;
+    box-sizing: border-box;
+  }
+  textarea {
+    border: 1px solid #ddd;
+    padding: 10px;
+    width: 100%;
+    box-sizing: border-box;
+    height: 100px;
+  }
+  form button {
+    display: block;
+    margin: 20px auto 0;
+    background: #00ce89;
+    color: white;
+    padding: 10px;
+    border: 0;
+    border-radius: 6px;
+    font-size: 16px;
+  }
+</style>
